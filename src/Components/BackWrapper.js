133d48a1ad1a85c1d9339a0330eee118d880436d
springhack <springhack@live.cn>
2017-05-31 21:34:58 +0800

    NavBar

--- src/Components/BackWrapper.js
+++ src/Components/BackWrapper.js
@@ -1,11 +1,14 @@
 /**
         Author: SpringHack - springhack@live.cn
-        Last modified: 2017-05-28 09:55:49
-        Filename: BackWrapper.js
+        Last modified: 2017-05-31 21:35:33
+        Filename: src/Components/BackWrapper.js
         Description: Created by SpringHack using vim automatically.
 **/
 import React, { Component } from 'react';
-import { View, BackHandler } from 'react-native';
+import { View, Text, TouchableHighlight, BackHandler } from 'react-native';
+import { Flex } from 'antd-mobile';
+
+import Style from '../Styles';
 
 let isSet = false;
 let lastBack = 0;
@@ -25,6 +28,23 @@ export default class BackWrapper extends Component {
     });
   }
   render() {
-    return (<View />);
+    if (this.props.location.pathname === '/') {
+      return (<View />);
+    }
+    return (
+      <Flex style={Style.navBar}>
+        <Flex.Item style={Style.navBarBack}>
+          <TouchableHighlight underlayColor='#fff' onPress={() => this.props.history.goBack()}>
+            <Text>{'< 返回'}</Text>
+          </TouchableHighlight>
+        </Flex.Item>
+        <Flex.Item>
+          <Text style={Style.navBarTitle}>{this.props.location.pathname}</Text>
+        </Flex.Item>
+        <Flex.Item style={Style.navBarBack}>
+          <View />
+        </Flex.Item>
+      </Flex>
+    );
   }
 }
--- src/Styles/index.js
+++ src/Styles/index.js
@@ -1,7 +1,7 @@
 /**
         Author: TanDV7 - tandv7@outlook.com
-        Last modified: 2017-05-29 00:11:47
-        Filename: index.js
+        Last modified: 2017-05-31 21:35:17
+        Filename: src/Styles/index.js
         Description: Created by TanDV7 using vim automatically.
 **/
 import { StyleSheet } from 'react-native';
@@ -9,5 +9,17 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   blankBorder: {
     margin: 10
+  },
+  navBar: {
+    padding: 10,
+    borderColor: '#ccc',
+    borderBottomWidth: 1
+  },
+  navBarBack: {
+    flex: 0,
+    width: 40
+  },
+  navBarTitle: {
+    textAlign: 'center'
   }
 });
